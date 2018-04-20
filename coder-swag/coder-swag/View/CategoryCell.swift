3958cb662fa0e02872da265b9d795341e21bfa04
Stevensoncrash <sjreyes1@yahoo.com>
2018-04-20 10:51:53 -0700

    custom view table cells. Hardcoded the IBoutlets

---
+++ coder-swag/coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Stephen Reyes on 4/20/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
