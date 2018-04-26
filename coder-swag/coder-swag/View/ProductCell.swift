52778a177bf68d24fd4cf96ff8dcba2fce57276f
Stevensoncrash <sjreyes1@yahoo.com>
2018-04-25 18:44:14 -0700

    added first cell of collectionview table. Hooked up IBoutlets.

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Stephen Reyes on 4/25/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+    
+}
